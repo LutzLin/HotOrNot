2049a9fbb2bc07a49cd2b303d681fcf317cd01c4
Lutz <uveeu@student.kit.edu>
2017-03-19 08:01:26 +0100

    front end done

--- script.js
+++ script.js
@@ -1,7 +1,29 @@
+var URL = "";
+
 function hot() {
-	//send true to server
+	$.post(URL,true,success: function (msg){over(msg);});
 }
 
 function not() {
-	//send false to server
+	$.post(URL,false),success: function (msg){over(msg);};
+}
+
+function over(message) {
+	if (message === "You Win!" || message === "You Lose!") {
+	document.getElementById("postgame").innerHTML = message;
+	document.getElementById("postgame").style.visibility = "visible";
+	document.getElementById("hot").style.visibility = "hidden";
+	document.getElementById("not").style.visibility = "hidden";
+	document.getElementById("celeb").style.visibility = "hidden";
+	} else {
+		document.getElementById("celeb").src = message;	
+	}
+}
+
+function start() {
+	$.get(URL); 
+	document.getElementById("hot").style.visibility = "visible";
+	document.getElementById("not").style.visibility = "visible";
+	document.getElementById("celeb").style.visibility = "visible";
+	document.getElementById("start").style.visibility = "hidden";
 }
